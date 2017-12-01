9e1d6cecb970fa0f02f97d7cb1906b1ea0bdf34d
Miranda <miranda.stoddard@tds.net>
2017-11-30 22:38:38 -0800

    Made collision detection and setters

--- GroupProjectGame/Objects.cpp
+++ GroupProjectGame/Objects.cpp
@@ -1,5 +1,34 @@
 #include "Objects.h"
 
-void Objects::moveObject() {
-	//stuff
+void Object::moveObject() {
+	
+}
+
+void Object::setDirection(float newDirection) {
+	direction = newDirection;
+}
+
+void Object::setPosition(float newPosition[2]) {
+	position[1] = newPosition[1];
+	position[2] = newPosition[2];
+}
+
+void Object::setRadius(int newRadius) { // shouldn't change after initialization
+	radius = newRadius;
+}
+
+void Object::setSpeed(float newSpeed) {
+	speed = newSpeed;
+}
+
+bool Object::collision(Object other) { // assuming our hitboxes will be circles because easier
+	bool crash = false;
+	float dist1 = position[1] - other.position[1]; //finding distance between points
+	float dist2 = position[2] - other.position[2];
+	float num = dist1*dist1 + dist2*dist2;
+	float finalDist = pow(num, 0.5);
+	if (finalDist <= radius + other.radius) {
+		crash = true;
+	}
+	return crash;
 }
--- GroupProjectGame/Objects.h
+++ GroupProjectGame/Objects.h
@@ -1,16 +1,20 @@
 #pragma once 
 #include <iostream>
 #include <string>
+#include <SFML/Graphics.hpp>
+#include <math.h>
 
-class Objects {
+class Object {
 protected:
 	float direction;
-	float position[2];
-	int size;
+	float position[2]; // row then column
+	int radius; 
 	float speed;
-
-	void objectCreator();
 	void moveObject();
 public:
-
+	void setDirection(float newDirection);
+	void setPosition(float newPosition[2]);
+	void setRadius(int newRadius);
+	void setSpeed(float newSpeed);
+	bool collision(Object other);
 };
