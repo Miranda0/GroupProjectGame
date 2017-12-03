d8d84072b69c0ba8af730bc4c20b83af5457c9a4
Sl8er <slater.weinstock@wsu.edu>
2017-12-02 18:43:24 -0800

    trying to get healthkit to appear, help me.

--- GroupProjectGame/healthkit.cpp
+++ GroupProjectGame/healthkit.cpp
@@ -2,6 +2,17 @@
 using std::cout;
 using std::endl;
 
+healthkit::healthkit() :Object(10, 0)
+{
+	srand(time(NULL));
+	int position = rand() % 1280;
+	texture.loadFromFile("newlifekit.png");
+	graphic->setPosition(position, 0);
+	graphic->setTexture(texture);
+	direction = rand() % 720;
+	std::cout << "this worked and the life kit has saved someone" << std::endl;
+
+}
 
 healthkit::~healthkit()
 {
@@ -10,19 +21,10 @@ healthkit::~healthkit()
 
 void healthkit::destroy()
 {
-	texture.loadFromFile("newlifekit.png");
+	
 }
 
-healthkit::healthkit():Object(1, 0) {
-	srand(time(NULL));
-	int position = rand() % 1280;
-	std::cout << "Position: " << position << std::endl;
-	texture.loadFromFile("newlifekit.png");
-	graphic->setPosition(position, 0);
-	graphic->setTexture(texture);
-	direction = rand() % 360;
-	std::cout << "Direction: " << direction << std::endl;
-}
+
 
 sf::Sprite* healthkit::getGraphic()
 {
