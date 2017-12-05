d11555016bfb072af51fa7ecc566c35a0f9796a7
Arthur Lang <aargolang@gmail.com>
2017-12-04 22:14:21 -0800

    got basic collisions and bullet shooting

--- GroupProjectGame/Asteroid.cpp
+++ GroupProjectGame/Asteroid.cpp
@@ -3,12 +3,22 @@
 Asteroid::Asteroid():Object(40, 5) {
 	srand (time(NULL));
 	int position = rand() % 1280;
-	std::cout << "Position: " << position << std::endl;
+	std::cout << "Position: " << position << std::endl;		// debug
 	texture.loadFromFile("asteroid-big-0000.png");
 	graphic->setPosition(position, 0);
 	graphic->setTexture(texture);
 	direction = rand() % 360;
-	std::cout << "Direction: " << direction << std::endl;
+	std::cout << "Direction: " << direction << std::endl;	// debug
+}
+Asteroid::Asteroid(float xPos, float yPos, float initSpeed, int initDirection) : Object(0, initSpeed)
+{
+	texture.loadFromFile("asteroid-big-0000.png");
+	graphic->setTexture(texture);
+	graphic->setPosition(xPos, yPos);
+	direction = initDirection;
+	collisionBox.setSize(sf::Vector2f(texture.getSize().x - 20.0f, texture.getSize().y-20.0f));
+	collisionBox.setPosition(xPos +10, yPos + 10);
+	collisionBox.setFillColor(sf::Color::White);
 }
 
 void Asteroid::moveObject(){
@@ -19,10 +29,14 @@ void Asteroid::moveObject(){
 	xMove = cos(radDir) * speed;
 	yMove = sin(radDir) * speed;
 	graphic->move(xMove, yMove);
+	collisionBox.move(xMove, yMove);
 }
 
 sf::Sprite* Asteroid::getGraphic() {
 	return graphic;
+}
+sf::RectangleShape Asteroid::getCollisionBox() {
+	return collisionBox;
 }
 
 Asteroid::~Asteroid() {
